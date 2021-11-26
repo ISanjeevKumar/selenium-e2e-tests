1917d0b65e9495a5047b8c7d45007a516c127581
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-26 23:23:53 +0530

    [Sanjeev] setup project  skelton

--- src/test/java/selenium/e2e/test/tests/smoke/LoginTest.java
+++ src/test/java/selenium/e2e/test/tests/smoke/LoginTest.java
@@ -2,14 +2,20 @@ package selenium.e2e.test.tests.smoke;
 
 import org.testng.annotations.Test;
 
+import selenium.e2e.test.enums.Browser;
+import selenium.e2e.test.pageObjects.SauceLabApp;
 import selenium.e2e.test.tests.TestBase;
+import selenium.e2e.test.tests.TestDataProvider;
 
 public class LoginTest extends TestBase {
 
-    @Test(description = "Check user should not be able to login with invalid password")
-    public void shouldNotBeAbleToLoginWithInvalidCredentials() {
+    @Test(description = "Check user should not be able to login with invalid password", dataProvider = "userdata", dataProviderClass = TestDataProvider.class)
+    public void shouldNotBeAbleToLoginWithInvalidCredentials(String username, String password) {
         try {
-            System.out.println("Login test");
+            System.out.println(username + ":" + password);
+            SauceLabApp sauceLabApp = new SauceLabApp(Browser.CHROME);
+            sauceLabApp.loginPage().login(username, password);
+
         } catch (Exception e) {
             System.err.println(e.getStackTrace());
         }
